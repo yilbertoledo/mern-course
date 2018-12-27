55f5259496156c7cacb338f206cc6de52fd50d30
Yilber Toledo <yilber.toledo@gmail.com>
2018-12-27 15:01:45 -0300

    index.js modified

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ hello: "world" });
 });
 
 //Cargar el puerto de forma dinámica (para Heroku) o estática (desarrollo)
